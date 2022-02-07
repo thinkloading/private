ee52dde25d2711e743f8304136ddea06df83db8f
thinkloading <90131558+thinkloading@users.noreply.github.com>
2022-02-07 19:01:23 +0800

    Update __init__.py

--- private/__init__.py
+++ private/__init__.py
@@ -1,12 +1,19 @@
 import string
 
 def switch_dict(raw_headers):
+    
+    '''将键值对转换成字典'''
+    
     data = [line.split(": ",1) for line in raw_headers.strip().split("\n")]
     headers = dict((x for x in data))
     return headers
 
-letters = string.ascii_letters + string.digits + "#"
+
 def Form(file_name):
+    
+    '''识别汉字，注释代码'''
+    
+    letters = string.ascii_letters + string.digits + "#"
     with open(file_name,"r",encoding="utf-8") as f:
         file_re = f.readlines()
     with open(file_name,"w+",encoding="utf-8") as f:
